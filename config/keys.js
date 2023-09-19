3074548ebf0e96a5228fc27818c8279a7265dccc
imaneiqordev <105134607+imaneiqordev@users.noreply.github.com>
2023-09-19 02:18:39 +0100

    third

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID:
+    "339156355161-88dql96hboq6dbinkc53efvinco79cr0.apps.googleusercontent.com",
+  googleClientSecret: "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",
+  cookieKey: "kjdfyudsvbhdsbvlu",
+};
